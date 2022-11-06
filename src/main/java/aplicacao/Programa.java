2ad308ae9cdf698aeb3be330346bb14abdc0ab4a
Jorge Denys <jorgedenys@gmail.com>
2022-11-06 12:39:01 -0300

    finalizando o projeto

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,9 +1,49 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		// Criando a tabela e persistindo dados
+		/*
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+		
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		*/
+		
+		// consultando dados da tabela
+		/*
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
+		*/
+		
+		// removendo dados da tabela
+		/*
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		*/
+		
+		System.out.println("Pronto!");
+		
+		em.close();
+		emf.close();
 		
 	}
 
